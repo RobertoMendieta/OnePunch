297696374b0ffca3bc082f6be6e3b6f68b8aab2a
lilli <lilli@LAPTOP-HUVQUDPM>
2022-10-06 17:24:58 -0600

    Agrege las vistas

---
+++ onePunch/onePunch/Controllers/PersonasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using onePunch;
+
+namespace onePunch.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private onePunchEntities1 db = new onePunchEntities1();
+
+        // GET: Personas
+        public ActionResult Index()
+        {
+            return View(db.Persona.ToList());
+        }
+
+        // GET: Personas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idPersona,nombres,apellidos,direccion,email,fechaNacimiento,telefono")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persona.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: Personas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idPersona,nombres,apellidos,direccion,email,fechaNacimiento,telefono")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Persona persona = db.Persona.Find(id);
+            db.Persona.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ onePunch/onePunch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="onePunch.MvcApplication" Language="C#" %>
